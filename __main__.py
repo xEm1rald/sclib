c6ac2303f3da4d80106967090256c0e5ea82d78d
xEm1rald <68512753+xEm1rald@users.noreply.github.com>
2025-09-29 20:04:44 +0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,6 +1,25 @@
 import subprocess
 import sys
+import ensurepip
+import os
+
+def ensure_pip():
+    try:
+        import pip  # noqa
+    except ImportError:
+        print("pip not found, bootstrapping with ensurepip...")
+        ensurepip.bootstrap()
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+
+
+def install_requirements():
+    pkgs = ["streamlit", "mutagen", "beautifulsoup4"]
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade"] + pkgs)
 
 
 if __name__ == "__main__":
-    subprocess.run([sys.executable, "-m", "streamlit", "run", r".\app\main.py"])
+    ensure_pip()
+    install_requirements()
+
+    app_path = os.path.join("app", "main.py")
+    subprocess.run([sys.executable, "-m", "streamlit", "run", app_path])
